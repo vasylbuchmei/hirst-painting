6928daa51131be1d31fc7cd24740861718ddb638
Vasyl Buchmei <77201726+vasylbuchmei@users.noreply.github.com>
2021-02-07 15:58:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-### Extracting color from the image ###
-# import colorgram
-# colors = colorgram.extract("image.jpg", 30)
-# rgb_colors = []
-# for color in colors:
-#     r = color.rgb.r
-#     g = color.rgb.g
-#     b = color.rgb.b
-#     new_color = (r, g, b)
-#     rgb_colors.append(new_color)
 
 import turtle as turtle_module
 import random
